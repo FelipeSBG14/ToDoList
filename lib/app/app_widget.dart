fa37f6d8feca8bf1a20f0864379beb21ff89d975
FelipeSBG14 <borges.felipe.felipe14@gmail.com>
2023-02-27 23:51:36 -0400

    first commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
+import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  var sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth auth = FirebaseAuth.instance;
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List Provider',
+      initialRoute: '/login',
+      theme: TodoListUiConfig.theme,
+      routes: {
+        ...AuthModule().routers,
+      },
+      home: const SplashPage(),
+    );
+  }
+}
---
+++ lib/app/core/widget/todo_list_field.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class TodoListField extends StatelessWidget {
+  final String label;
+  final IconButton? suffixIconButton;
+  final bool obscureText;
+  final ValueNotifier<bool> obscureTextVN;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+
+  TodoListField({
+    Key? key,
+    required this.label,
+    this.suffixIconButton,
+    this.obscureText = false,
+    this.controller,
+    this.validator,
+  })  : assert(obscureText == true ? suffixIconButton == null : true,
+            'obscuteText Não pode ser enviado em conjunto com suffixIconButton'),
+        obscureTextVN = ValueNotifier(obscureText),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVN,
+      builder: (_, obscureTextValue, child) {
+        return TextFormField(
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                30,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(
+                30,
+              ),
+              borderSide: const BorderSide(
+                color: Colors.red,
+              ),
+            ),
+            isDense: true,
+            suffixIcon: suffixIconButton ??
+                (obscureText == true
+                    ? IconButton(
+                        icon: Icon(
+                          !obscureTextValue
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          obscureTextVN.value = !obscureTextValue;
+                        },
+                      )
+                    : null),
+          ),
+          obscureText: obscureTextValue,
+        );
+      },
+    );
+  }
+}
