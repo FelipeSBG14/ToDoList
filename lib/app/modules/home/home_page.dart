95b81ca15915e055a50d5fe88d0d86e064f3b674
FelipeSBG14 <borges.felipe.felipe14@gmail.com>
2023-03-16 16:40:01 -0400

    diferenca no resultado

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,16 +11,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        iconTheme: IconThemeData(color: context.primaryColor),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.filter_alt_rounded,
+            ),
+            itemBuilder: (_) => [
+              const PopupMenuItem<bool>(
+                child: Text('Mostrar tarefas concluídas'),
+              ),
+            ],
+          )
+        ],
       ),
       drawer: HomeDrawer(),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            context.read<AuthProvider>().logout();
-          },
-          child: Text('Logout'),
-        ),
+      body: LayoutBuilder(
+        builder: (context, constrains) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constrains.maxHeight,
+                minWidth: constrains.maxWidth,
+              ),
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      HomeHeader(),
+                      HomeFilters(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
